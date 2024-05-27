f0ae8c41a3cb24b5317336573829c611e9c6f7b1
gabims029 <gabimelo541@gmail.com>
2024-05-27 11:28:54 -0300

    parte1 exemplos listas

---
+++ Exemplos_27_05/lista1.py
@@ -0,0 +1,23 @@
+import time
+
+numeros = [111, 7, 1 ,1]
+print(len(numeros))
+
+numeros.append(52)
+print(len(numeros))
+print(numeros)
+
+numeros.insert(0, 222)
+print(len(numeros))
+print(numeros)
+
+numeros.insert(-1, 12)
+print(numeros)
+
+numeros.insert(1, 18)
+print(numeros)
+
+numeros.insert(-2, 20)
+print(numeros)
+
+time.sleep(2)
